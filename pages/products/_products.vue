d41a3df1ae5b0fdb239886928bd57088288d0a85
Godwin Alexander <alecgee73@gmail.com>
2020-12-20 02:41:11 +0100

    app update

--- pages/products/_products.vue
+++ pages/products/_products.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Nav class="sticky top-0" />
-    <h1 class="font-bold m-5 md:mx-10">{{ data.name }}</h1>
+    <h1 class="font-bold m-5 md:mx-10">{{ currentProduct.name }}</h1>
     <Products :data="currentProduct" />
     <Ads class="mx-auto sm:m-10" />
     <Footer />
