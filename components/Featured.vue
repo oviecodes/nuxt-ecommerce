7cba39c3b5da73148a84f972bc861e2f85069baa
Godwin Alexander <alecgee73@gmail.com>
2020-12-21 03:33:31 +0100

    ready to test production

--- components/Featured.vue
+++ components/Featured.vue
@@ -11,7 +11,7 @@
         <div class="img-wrapper h-3/4 mx-auto max-h-screen">
           <img
             class="flex-shrink h-1/2"
-            :src="`https://enigmatic-peak-00809.herokuapp.com${product.Image[0].url}`"
+            :src="`${product.Image[0].url}`"
             alt=""
           />
         </div>
