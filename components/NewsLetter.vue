17f89cedfd4aa42a4550ada6f5b378b693bae9cf
Godwin Alexander <alecgee73@gmail.com>
2021-01-04 18:59:09 +0100

    more changes

--- components/NewsLetter.vue
+++ components/NewsLetter.vue
@@ -6,15 +6,17 @@
       <h1 class="text-lg m-7">Sign Up For NewsLetter</h1>
     </div>
     <div>
-      <input
-        id=""
-        v-model="email"
-        class="p-2 m-3 sm:m-0 border border-solid border-t-0 border-l-0 border-r-0 border-b-1 outline-none border-black"
-        type="email"
-        name=""
-        placeholder="email"
-      />
-      <button class="button--grey" @click="handleSuscribe()">Subscribe</button>
+      <form @submit="handleSuscribe()" >
+        <input
+          id=""
+          v-model="email"
+          class="p-2 m-3 sm:m-0 border border-solid border-t-0 border-l-0 border-r-0 border-b-1 outline-none border-black"
+          type="email"
+          name=""
+          placeholder="email"
+        />
+        <button type="submit" class="button--grey">Subscribe</button>
+      </form>
     </div>
   </div>
 </template>
@@ -28,7 +30,8 @@ export default {
     }
   },
   methods: {
-    async handleSuscribe() {
+    async handleSuscribe(e) {
+      e.preventDefault()
       this.$swal({
         title: 'Successful!',
         text: 'Thanks for Subscribing',
@@ -36,6 +39,7 @@ export default {
         button: 'Ok',
       })
       await this.$strapi.$subscribers.create({ Email: this.email })
+      //clear email input
       this.email = ''
     },
   },
